7043800c9ae59f08cb690c786638547f5cc58259
stom01 <skylertom@gmail.com>
2014-03-10 02:08:09 -0400

    Made info Windows

--- transit/map.js
+++ transit/map.js
@@ -178,7 +178,6 @@ function findStation(callback) {
 	var i = 0;
 	console.log("there are " + markers.length + " items in the list");
 	markers.map(function (item) {
-		console.log("running");
 		var lat2 = initialLocation.lat(); 
 		var lon2 = initialLocation.lng(); 
 		var R = 6371; // km 

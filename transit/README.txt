f63fb86648734d230aba558ebb270bbc6a751144
stom01 <skylertom@gmail.com>
2014-03-10 00:07:45 -0400

    Made info Windows

--- transit/README.txt
+++ transit/README.txt
@@ -1,2 +1,5 @@
 Skyler Tom
 
+1. The lines are not connected, trouble with inbound and outbound, you are here marker doesn't yet have an info window, have not calculated the nearest T stop, css file isn't complete
+2. I have collaborated with: 
+3. I've spent 10 hours (so far)
--- transit/map.js
+++ transit/map.js
@@ -1,9 +1,13 @@
+var midpoint;
 var xhr;
 var initialLocation;
 var initialMarker;
+var scheduleData;
+var markers = [];
 var campusCenter = new google.maps.LatLng(42.405816, -71.120443);
 var lineData = [];
 var map;
+var SEC = 60;
 function init() {
 	xhr = new XMLHttpRequest();
 	xhr.open("get", "http://mbtamap.herokuapp.com/mapper/rodeo.json", true);
@@ -37,6 +41,7 @@ function init() {
 google.maps.event.addDomListener(window, 'load', init);
 
 function handleNoGeolocation(errorFlag) {
+	map.setZoom(12);
 	if (errorFlag == true) {
 		alert("Geolocation service failed.");
 		initialLocation = campusCenter;
@@ -50,24 +55,21 @@ function handleNoGeolocation(errorFlag) {
 function dataReady() {
 	if (xhr.readyState == 4 && xhr.status == 200) {
 		scheduleData = JSON.parse(xhr.responseText);
-		scheduleDom = document.getElementById("schedule");
-		scheduleDom.innerHTML = scheduleData["line"];
+		console.log(scheduleData["line"]);
 		getLine(scheduleData["line"]);
 		MakeLineMarkers();
 		drawLine(scheduleData["line"]);
 	}
 	else if (xhr.readyState == 4 && xhr.status == 500) {
-		scheduleDom = document.getElementById("schedule");
+		scheduleDom = document.getElementById("map_canvas");
 		scheduleDom.innerHTML = '<p>Could not access MBTA schedule</p>';
 	}
 }
 
-//orange line is vertical
-//blue line is sort of a "J" shape
-//red line splits at bottom (an upside down "Y" shape)
 function getLine(line) {
 	line = line.replace(line[0], line[0].toUpperCase());
-	var trainsFile = "Blue,Airport,42.374262,-71.030395-- Blue,Aquarium,42.359784,-71.051652-- Blue,Beachmont,42.39754234,-70.99231944-- Blue,Bowdoin,42.361365,-71.062037-- Blue,Government Center,42.359705,-71.05921499999999-- Blue,Maverick,42.36911856,-71.03952958000001-- Blue,Orient Heights,42.386867,-71.00473599999999-- Blue,Revere Beach,42.40784254,-70.99253321-- Blue,State Street,42.358978,-71.057598-- Blue,Suffolk Downs,42.39050067,-70.99712259-- Blue,Wonderland,42.41342,-70.991648-- Blue,Wood Island,42.3796403,-71.02286539000001-- Orange,Back Bay,42.34735,-71.075727-- Orange,Chinatown,42.352547,-71.062752-- Orange,Community College,42.373622,-71.06953300000001-- Orange,Downtown Crossing,42.355518,-71.060225-- Orange,Forest Hills,42.300523,-71.113686-- Orange,Green Street,42.310525,-71.10741400000001-- Orange,Haymarket,42.363021,-71.05829-- Orange,Jackson Square,42.323132,-71.099592-- Orange,Malden Center,42.426632,-71.07411-- Orange,Mass Ave,42.341512,-71.083423-- Orange,North Station,42.365577,-71.06129-- Orange,Oak Grove,42.43668,-71.07109699999999-- Orange,Roxbury Crossing,42.331397,-71.095451-- Orange,Ruggles,42.336377,-71.088961-- Orange,State Street,42.358978,-71.057598-- Orange,Stony Brook,42.317062,-71.104248-- Orange,Sullivan,42.383975,-71.076994-- Orange,Tufts Medical,42.349662,-71.063917-- Orange,Wellington,42.40237,-71.077082-- Red,Alewife,42.395428,-71.142483-- Red,Andrew,42.330154,-71.057655-- Red,Ashmont,42.284652,-71.06448899999999-- Red,Braintree,42.2078543,-71.0011385-- Red,Broadway,42.342622,-71.056967-- Red,Central Square,42.365486,-71.103802-- Red,Charles/MGH,42.361166,-71.070628-- Red,Davis,42.39674,-71.121815-- Red,Downtown Crossing,42.355518,-71.060225-- Red,Fields Corner,42.300093,-71.061667-- Red,Harvard Square,42.373362,-71.118956-- Red,JFK/UMass,42.320685,-71.052391-- Red,Kendall/MIT,42.36249079,-71.08617653-- Red,North Quincy,42.275275,-71.029583-- Red,Park Street,42.35639457,-71.0624242-- Red,Porter Square,42.3884,-71.11914899999999-- Red,Quincy Adams,42.233391,-71.007153-- Red,Quincy Center,42.251809,-71.005409-- Red,Savin Hill,42.31129,-71.053331-- Red,Shawmut,42.29312583,-71.06573796000001-- Red,South Station,42.352271,-71.05524200000001-- Red,Wollaston,42.2665139,-71.0203369--"
+	//var trainsFile = "Blue,Airport,42.374262,-71.030395-- Blue,Aquarium,42.359784,-71.051652-- Blue,Beachmont,42.39754234,-70.99231944-- Blue,Bowdoin,42.361365,-71.062037-- Blue,Government Center,42.359705,-71.05921499999999-- Blue,Maverick,42.36911856,-71.03952958000001-- Blue,Orient Heights,42.386867,-71.00473599999999-- Blue,Revere Beach,42.40784254,-70.99253321-- Blue,State Street,42.358978,-71.057598-- Blue,Suffolk Downs,42.39050067,-70.99712259-- Blue,Wonderland,42.41342,-70.991648-- Blue,Wood Island,42.3796403,-71.02286539000001-- Orange,Back Bay,42.34735,-71.075727-- Orange,Chinatown,42.352547,-71.062752-- Orange,Community College,42.373622,-71.06953300000001-- Orange,Downtown Crossing,42.355518,-71.060225-- Orange,Forest Hills,42.300523,-71.113686-- Orange,Green Street,42.310525,-71.10741400000001-- Orange,Haymarket,42.363021,-71.05829-- Orange,Jackson Square,42.323132,-71.099592-- Orange,Malden Center,42.426632,-71.07411-- Orange,Mass Ave,42.341512,-71.083423-- Orange,North Station,42.365577,-71.06129-- Orange,Oak Grove,42.43668,-71.07109699999999-- Orange,Roxbury Crossing,42.331397,-71.095451-- Orange,Ruggles,42.336377,-71.088961-- Orange,State Street,42.358978,-71.057598-- Orange,Stony Brook,42.317062,-71.104248-- Orange,Sullivan,42.383975,-71.076994-- Orange,Tufts Medical,42.349662,-71.063917-- Orange,Wellington,42.40237,-71.077082-- Red,Alewife,42.395428,-71.142483-- Red,Andrew,42.330154,-71.057655-- Red,Ashmont,42.284652,-71.06448899999999-- Red,Braintree,42.2078543,-71.0011385-- Red,Broadway,42.342622,-71.056967-- Red,Central Square,42.365486,-71.103802-- Red,Charles/MGH,42.361166,-71.070628-- Red,Davis,42.39674,-71.121815-- Red,Downtown Crossing,42.355518,-71.060225-- Red,Fields Corner,42.300093,-71.061667-- Red,Harvard Square,42.373362,-71.118956-- Red,JFK/UMass,42.320685,-71.052391-- Red,Kendall/MIT,42.36249079,-71.08617653-- Red,North Quincy,42.275275,-71.029583-- Red,Park Street,42.35639457,-71.0624242-- Red,Porter Square,42.3884,-71.11914899999999-- Red,Quincy Adams,42.233391,-71.007153-- Red,Quincy Center,42.251809,-71.005409-- Red,Savin Hill,42.31129,-71.053331-- Red,Shawmut,42.29312583,-71.06573796000001-- Red,South Station,42.352271,-71.05524200000001-- Red,Wollaston,42.2665139,-71.0203369--"
+	var trainsFile = "Blue,Bowdoin,42.361365,-71.062037-- Blue,Government Center,42.359705,-71.05921499999999-- Blue,State Street,42.358978,-71.057598-- Blue,Aquarium,42.359784,-71.051652-- Blue,Maverick,42.36911856,-71.03952958000001-- Blue,Airport,42.374262,-71.030395-- Blue,Wood Island,42.3796403,-71.02286539000001-- Blue,Orient Heights,42.386867,-71.00473599999999-- Blue,Suffolk Downs,42.39050067,-70.99712259-- Blue,Beachmont,42.39754234,-70.99231944-- Blue,Revere Beach,42.40784254,-70.99253321-- Blue,Wonderland,42.41342,-70.991648-- Orange,Oak Grove,42.43668,-71.07109699999999-- Orange,Malden Center,42.426632,-71.07411-- Orange,Wellington,42.40237,-71.077082-- Orange,Sullivan,42.383975,-71.076994-- Orange,Community College,42.373622,-71.06953300000001-- Orange,North Station,42.365577,-71.06129-- Orange,Haymarket,42.363021,-71.05829-- Orange,State Street,42.358978,-71.057598-- Orange,Downtown Crossing,42.355518,-71.060225-- Orange,Chinatown,42.352547,-71.062752-- Orange,Tufts Medical,42.349662,-71.063917-- Orange,Back Bay,42.34735,-71.075727-- Orange,Mass Ave,42.341512,-71.083423-- Orange,Ruggles,42.336377,-71.088961-- Orange,Roxbury Crossing,42.331397,-71.095451-- Orange,Jackson Square,42.323132,-71.099592-- Orange,Stony Brook,42.317062,-71.104248-- Orange,Green Street,42.310525,-71.10741400000001-- Orange,Forest Hills,42.300523,-71.113686--";
 	var trainsParsed = trainsFile.split('-- ');
 	var inputArray;
 	var i = 0;
@@ -86,6 +88,7 @@ function getLine(line) {
 function MakeLineMarkers() {
 	var lineMarkers;
 	var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+	var i = 0;
 	lineData.map(function(item) {
 		lineMarkers = new google.maps.Marker ({
 			position: new google.maps.LatLng(item["lat"], item["lng"]),
@@ -94,19 +97,31 @@ function MakeLineMarkers() {
 			title:  item["place"],
 			animation: google.maps.Animation.DROP
 		});
+		markers[i++] = lineMarkers;
 		displayInfo(lineMarkers);
 	});
 }
 
 function displayInfo(myMarker) {
-	// holds array of trains: lineData["schedule"]
 	var contentString = 
-		'<h1>' + myMarker.getTitle() + '</h1>' +
-		'<div class="inbound"> <h2>Inbound</h2>' +
-		'</div>'
-		'<div class="outbound"> <h2>Outbound</h2>' +
-		'</div>'
-		;
+		'<h1>' + myMarker.getTitle() + '</h1><table><tr><th>Line:</th><th>TrainID:</th><th>Destination:</th><th>Arriving in:</th></tr>';
+		scheduleData["schedule"].map(function(train) {
+			var time = "";
+			var trainID = "";
+			var destination = "";
+			train["Predictions"].map(function(stops) {
+				if (myMarker.getTitle() == stops["Stop"]) {
+					time = Math.floor(stops["Seconds"]/SEC) + "m"+stops["Seconds"]%SEC + "s";
+					trainID = train["TripID"];
+					destination = train["Destination"];
+				}
+			});
+			if (trainID != "") {
+				var addString = '<tr><td>' + scheduleData["line"] + '</td><td>' + trainID + '</td><td>' + 
+						destination + '</td><td>' + time + '</td></tr>';
+				contentString = contentString + addString;
+			}
+		});
 	var infowindow = new google.maps.InfoWindow({
 		content: contentString
 	});
@@ -136,4 +151,42 @@ function drawLine(line) {
 		strokeWeight: 2
 	});
 	linePath.setMap(map);
+}
+function findStation() {
+	Number.prototype.toRad = function() {
+		return this * Math.PI / 180;
+	}
+
+	var i = 0;
+	var distances = [];
+	var lat2 = initialLocation.lat(); 
+	var lon2 = initialLocation.lng(); 
+	var R = 6371; // km 
+	markers.map(function (item) {
+		var lat1 = item.getPosition().lat();
+		var long1 = item.getPosition().lng();
+		var x1 = lat2-lat1;
+		var dLat = x1.toRad();  
+		var x2 = lon2-lon1;
+		var dLon = x2.toRad();  
+		var a = Math.sin(dLat/2) * Math.sin(dLat/2) + 
+		                Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) * 
+		                Math.sin(dLon/2) * Math.sin(dLon/2);  
+		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+		distances[i++] = R * c; 
+	});
+	var index = findIndexOfMin(distances);
+	console.log(markers[index].getTitle());
+}
+
+function findIndexOfMin(array) {
+	var minIndex;
+	var currentMin;
+	for (var i in array) {
+		if (array[i] < currentMin) {
+			currentMin = array[i];
+			minIndex = i;
+		}
+	}
+	return i;
 }

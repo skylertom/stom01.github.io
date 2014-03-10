b028d0824396f3984e20123ab8f5262fac571695
stom01 <skylertom@gmail.com>
2014-03-10 04:04:17 -0400

    red line implemented, just need inbound/outbound calculations

--- transit/README.txt
+++ transit/README.txt
@@ -3,5 +3,9 @@ Skyler Tom
 1. Red line not implemented,
 	Blue line: good
 	Orange Line: good
+	figure out inbound and outbound
 2. I have collaborated with: 
-3. I've spent 10 hours (so far)
+3. I've spent 16 hours (so far)
+
+
+Plan for the redline:
--- transit/map.js
+++ transit/map.js
@@ -87,7 +87,7 @@ function handleNoGeolocation(errorFlag) {
 
 function getLine(line, callback) {
 	line = line.replace(line[0], line[0].toUpperCase());
-	var trainsFile = "Blue,Bowdoin,42.361365,-71.062037-- Blue,Government Center,42.359705,-71.05921499999999-- Blue,State Street,42.358978,-71.057598-- Blue,Aquarium,42.359784,-71.051652-- Blue,Maverick,42.36911856,-71.03952958000001-- Blue,Airport,42.374262,-71.030395-- Blue,Wood Island,42.3796403,-71.02286539000001-- Blue,Orient Heights,42.386867,-71.00473599999999-- Blue,Suffolk Downs,42.39050067,-70.99712259-- Blue,Beachmont,42.39754234,-70.99231944-- Blue,Revere Beach,42.40784254,-70.99253321-- Blue,Wonderland,42.41342,-70.991648-- Orange,Oak Grove,42.43668,-71.07109699999999-- Orange,Malden Center,42.426632,-71.07411-- Orange,Wellington,42.40237,-71.077082-- Orange,Sullivan,42.383975,-71.076994-- Orange,Community College,42.373622,-71.06953300000001-- Orange,North Station,42.365577,-71.06129-- Orange,Haymarket,42.363021,-71.05829-- Orange,State Street,42.358978,-71.057598-- Orange,Downtown Crossing,42.355518,-71.060225-- Orange,Chinatown,42.352547,-71.062752-- Orange,Tufts Medical,42.349662,-71.063917-- Orange,Back Bay,42.34735,-71.075727-- Orange,Mass Ave,42.341512,-71.083423-- Orange,Ruggles,42.336377,-71.088961-- Orange,Roxbury Crossing,42.331397,-71.095451-- Orange,Jackson Square,42.323132,-71.099592-- Orange,Stony Brook,42.317062,-71.104248-- Orange,Green Street,42.310525,-71.10741400000001-- Orange,Forest Hills,42.300523,-71.113686";
+	var trainsFile = "Blue,Bowdoin,42.361365,-71.062037-- Blue,Government Center,42.359705,-71.05921499999999-- Blue,State Street,42.358978,-71.057598-- Blue,Aquarium,42.359784,-71.051652-- Blue,Maverick,42.36911856,-71.03952958000001-- Blue,Airport,42.374262,-71.030395-- Blue,Wood Island,42.3796403,-71.02286539000001-- Blue,Orient Heights,42.386867,-71.00473599999999-- Blue,Suffolk Downs,42.39050067,-70.99712259-- Blue,Beachmont,42.39754234,-70.99231944-- Blue,Revere Beach,42.40784254,-70.99253321-- Blue,Wonderland,42.41342,-70.991648-- Orange,Oak Grove,42.43668,-71.07109699999999-- Orange,Malden Center,42.426632,-71.07411-- Orange,Wellington,42.40237,-71.077082-- Orange,Sullivan,42.383975,-71.076994-- Orange,Community College,42.373622,-71.06953300000001-- Orange,North Station,42.365577,-71.06129-- Orange,Haymarket,42.363021,-71.05829-- Orange,State Street,42.358978,-71.057598-- Orange,Downtown Crossing,42.355518,-71.060225-- Orange,Chinatown,42.352547,-71.062752-- Orange,Tufts Medical,42.349662,-71.063917-- Orange,Back Bay,42.34735,-71.075727-- Orange,Mass Ave,42.341512,-71.083423-- Orange,Ruggles,42.336377,-71.088961-- Orange,Roxbury Crossing,42.331397,-71.095451-- Orange,Jackson Square,42.323132,-71.099592-- Orange,Stony Brook,42.317062,-71.104248-- Orange,Green Street,42.310525,-71.10741400000001-- Orange,Forest Hills,42.300523,-71.113686-- Red,Alewife,42.395428,-71.142483-- Red,Davis,42.39674,-71.121815-- Red,Porter Square,42.3884,-71.11914899999999-- Red,Harvard Square,42.373362,-71.118956-- Red,Central Square,42.365486,-71.103802-- Red,Kendall/MIT,42.36249079,-71.08617653-- Red,Charles/MGH,42.361166,-71.070628-- Red,Park Street,42.35639457,-71.0624242-- Red,Downtown Crossing,42.355518,-71.060225-- Red,South Station,42.352271,-71.05524200000001-- Red,Broadway,42.342622,-71.056967-- Red,Andrew,42.330154,-71.057655-- Red,JFK/UMass,42.320685,-71.052391-- Red,Savin Hill,42.31129,-71.053331-- Red,Fields Corner,42.300093,-71.061667-- Red,Shawmut,42.29312583,-71.06573796000001-- Red,Ashmont,42.284652,-71.06448899999999-- Red,North Quincy,42.275275,-71.029583-- Red,Wollaston,42.2665139,-71.0203369-- Red,Quincy Center,42.251809,-71.005409-- Red,Quincy Adams,42.233391,-71.007153-- Red,Braintree,42.2078543,-71.0011385";
 	var trainsParsed = trainsFile.split('-- ');
 	var inputArray;
 	var i = 0;
@@ -164,14 +164,36 @@ function drawLine(line, callback) {
 		lineColor = '#FF4500';
 	}
 	var coords = [];
-	for (var i in lineData) {
-		coords[i] = new google.maps.LatLng(lineData[i]["lat"], lineData[i]["lng"]);
+	if (line != "red") {
+		for (var i in lineData) {
+			coords[i] = new google.maps.LatLng(lineData[i]["lat"], lineData[i]["lng"]);
+		}
+	} else {
+		var i = 0;
+		var fork = [];
+		while (lineData[i]["place"] != "North Quincy") {
+			coords[i] = new google.maps.LatLng(lineData[i]["lat"], lineData[i]["lng"]);
+			i++;
+		}
+		var j = 0;
+		indexCutoff = i - 5;
+		fork[j] = new google.maps.LatLng(lineData[indexCutoff]["lat"], lineData[indexCutoff]["lng"]);
+		for (j = 1; i < lineData.length; j++, i++) {
+			fork[j] = new google.maps.LatLng(lineData[i]["lat"], lineData[i]["lng"]);
+		}
+		var forkPath = new google.maps.Polyline({
+			path: fork,
+			geodesi : true,
+			strokeColor: lineColor,
+			strokeWeight: 4
+		});
+		forkPath.setMap(map);
 	}
 	var linePath = new google.maps.Polyline({
 		path: coords,
 		geodesi : true,
 		strokeColor: lineColor,
-		strokeWeight: 2
+		strokeWeight: 4
 	});
 	linePath.setMap(map);
 	if (callback) {
@@ -208,7 +230,7 @@ function findStation(callback) {
 	var minDist =Math.round(minDist * 100) / 100;
 	var infowindow = new google.maps.InfoWindow({
 		content: "<h3>You Are Here </h3><p>Nearest " + scheduleData["line"] + " line stop is " + 
-			markers[distanceInfo[0]].getTitle() + "(" + minDist + " miles away)</p>"
+			markers[distanceInfo[0]].getTitle() + "(~" + minDist + " miles away)</p>"
 	});
 
 	google.maps.event.addListener(initialMarker, 'click', function() {
